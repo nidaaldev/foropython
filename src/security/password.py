4ab224ee14ad32623f5e224dfbd1c33720dc6888
Nidal Dev <neonzdeveloper@gmail.com>
2026-01-14 17:41:44 +0100

    Implement password hashing and password verification with bcrypt

---
+++ src/security/password.py
@@ -0,0 +1,9 @@
+import bcrypt
+
+def hash_password(password: bytes):
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password=password, salt=salt)
+    return hashed_password
+
+def check_password(password: bytes, hashed_password: bytes):
+    return bcrypt.checkpw(password=password, hashed_password=hashed_password)

78cf410b5d5c8370cce206f1fb2c61b50c8d5f2b
Nidal Dev <neonzdeveloper@gmail.com>
2026-01-18 16:30:53 +0100

    refactor: replace ID with UUID to improve security

--- src/models/user.py
+++ src/models/user.py
@@ -4,10 +4,11 @@ from sqlalchemy.orm import Mapped, relationship
 from sqlalchemy.orm import mapped_column
 from ..models.thread import Thread
 from .base import Base
+from uuid import uuid4
 
 class User(Base):
     __tablename__="user"
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[str] = mapped_column(primary_key=True, default=lambda: str(uuid4()))
     email: Mapped[str] = mapped_column(unique=True, nullable=False)
     username: Mapped[str] = mapped_column(unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
